40b07f516a5760f5bf78c615dd24805809cd9ad9
connorcasner <hey@connorcasner.com>
2022-08-13 14:25:14 -0700

    feat:add groups

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+    config.action_mailer.default_url_options = {
+    host: ENV["APP_HOST"],
+    protocol: "https"
+  }
 end
